d1e6fef18378b054499f9572a5d06e12a42cde8b
yurikorantraining <118156517+yurikorantraining@users.noreply.github.com>
2022-11-14 06:28:05 +0000

    Update streamlit_app.py
    add 1 line

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('🥣Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
